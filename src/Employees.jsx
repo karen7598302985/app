a1bfd7cfb74dcb137ca5bfbdb13b36d3a23d3820
KarenPortillo2687351 <124842168+karen7598302985@users.noreply.github.com>
2023-08-29 15:13:47 -0500

    Update Employees.jsx

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -3,75 +3,82 @@ import './Employees.css';
 
 const Employees=()=>{
     const[employees, setEmployees] = useState([
-        {
-            id:1,
+         {id:1,
             fullname:"Pepe Perez",
             designation:"maestro",
             gender:"male",
-            teamName:"teamA",
+            teamName:"teamA"
+            },
 
-            id:2,
+            {id:2,
             fullname:"Andres Castillo",
             designation:"ingeniero",
             gender:"male",
-            teamName:"teamB",
+            teamName:"teamB"
+            },
 
-            id:3,
+            {id:3,
             fullname:"Aleja Diaz",
             designation:"Enfermera",
             gender:"famale",
-            teamName:"teamC",
+            teamName:"teamC"
+            },
 
-            id:4,
+            {id:4,
             fullname:"Carlos Peña",
             designation:"Abogado",
             gender:"male",
-            teamName:"teamA",
-
-            id:5,
+            teamName:"teamA"
+            },
+        
+            {id:5,
             fullname:"Felipe Cardenas",
             designation:"Doctor",
             gender:"male",
-            teamName:"teamB",
+            teamName:"teamB"
+            },
 
-            id:6,
+            {id:6,
             fullname:"Karol Ortiz",
             designation:"maestra",
             gender:"famale",
-            teamName:"teamB",
+            teamName:"teamB"
+            },
 
-            id:7,
+            {id:7,
             fullname:"Antonia Barreta",
             designation:"ingeniera",
             gender:"famale",
-            teamName:"teamC",
+            teamName:"teamC"
+            },
 
-            id:8,
+            {id:8,
             fullname:"Juan Calos",
             designation:"Arquitecto",
             gender:"male",
-            teamName:"teamA",
+            teamName:"teamA"
+            },
 
-            id:9,
+            {id:9,
             fullname:"Antonio Castro",
             designation:"Musico",
             gender:"male",
-            teamName:"teamA",
+            teamName:"teamA"
+            },
 
-            id:10,
+            {id:10,
             fullname:"Santiago Murillo",
             designation:"Cocinero",
             gender:"male",
-            teamName:"teamB",
+            teamName:"teamB"
+            },
 
-            id:11,
+            {id:11,
             fullname:"Karen Savedra",
             designation:"Jugadora",
             gender:"famale",
-            teamName:"teamC",
-
-        }
-    
+            teamName:"teamC"
+            },
     ]);
     return (
         <main className="employee-list">
